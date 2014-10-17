f40c962e6d657c8d3063c79f732d1b7016c62da9
Daniel Perez <tuvistavie@gmail.com>
2014-10-18 02:05:11 +0900

    Add models.

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 var express  = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    db       = mongoose.connection;
 
 mongoose.connect('mongodb://localhost/blog_api');
-
-var db = mongoose.connection;
 
 db.on('error', function (err) {
   console.warn("Could not connect to mongodb");
   process.exit(1);
 });
+
+// var __options = mongoose.Schema.prototype.defaultOptions;
+// mongoose.Schema.prototype.defaultOptions = function (options) {
+//   options = __options.apply(this, arguments);
+//   options.toObject = options.toObject || {
+//     transform: function (doc, ret, options) {
+//       ret.id = ret._id;
+//       delete ret._id;
+//       return ret;
+//     }
+//   };
+//   return options;
+// };
 
 db.once('open', function () {
   var app = express();
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,13 @@
 var express = require('express'),
-    router  = express.Router();
+    router  = express.Router(),
+    models  = require('../models');
+
 
 router.get('/', function (req, res) {
-    res.json({router: 'posts', path: '/'});
+  models.Post.find(function (err, posts) {
+    if (err) return res.status(500).json({error: err.message});
+    res.json(posts);
+  });
 });
 
 module.exports = router;
