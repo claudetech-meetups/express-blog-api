646cce67fefc5807cf92346475ea7f0178057563
Daniel Perez <tuvistavie@gmail.com>
2014-10-18 15:02:30 +0900

    Write test to create post.

--- test/posts_test.js
+++ test/posts_test.js
@@ -98,17 +98,33 @@ describe('Posts', function () {
     it('should return existing post', function (done) {
       models.Post.create({title: "foo", content: "bar"}, function (err, expected) {
         request(app)
-        .get('/posts/' + expected.id.toString())
-        .expect(200)
-        .expect(function (res) {
-          post = res.body;
-          expect(post.id).to.eq(expected._id.toString());
-          expect(post.title).to.eq(expected.title);
-          expect(post.content).to.eq(expected.content);
-        })
-        .end(done);
+          .get('/posts/' + expected.id.toString())
+          .expect(200)
+          .expect(function (res) {
+            post = res.body;
+            expect(post.id).to.eq(expected._id.toString());
+            expect(post.title).to.eq(expected.title);
+            expect(post.content).to.eq(expected.content);
+          })
+          .end(done);
       });
     });
+  });
 
+  describe('POST /posts', function () {
+    it('should create Post', function (done) {
+      var postContent = {title: "foo", content: "bar"};
+      request(app)
+        .post('/posts')
+        .send(postContent)
+        .expect(201)
+        .expect(function (res) {
+          post = res.body;
+          expect(post.id).not.to.be.null;
+          expect(post.title).to.eq(postContent.title);
+          expect(post.content).to.eq(postContent.content);
+        })
+        .end(done);
+    });
   });
 });
