20a869f3d449093c3954efcb33c128c12677b643
Daniel Perez <tuvistavie@gmail.com>
2014-10-18 14:39:57 +0900

    Add get post test and get posts with category_id filter.

--- test/posts_test.js
+++ test/posts_test.js
@@ -3,19 +3,27 @@ var request = require('supertest'),
     config  = require('../config').test;
 
 describe('Posts', function () {
-  var app, models;
+  var app, models, categories;
 
   before(function (done) {
     require('../app')(config, function (err, a) {
-      models  = require('../models');
       if (err) return done(err);
+      models  = require('../models');
+      categories = [];
+      models.Category.create([{name: "foo"}, {name: "bar"}], function (err, cat1, cat2) {
+        categories.push(cat1);
+        categories.push(cat2);
+      });
       app = a;
       done();
     });
   });
 
-  after(function () {
-    require('mongoose').connection.close();
+  after(function (done) {
+    models.Category.remove({}, function (err) {
+      require('mongoose').connection.close();
+      done(err);
+    });
   });
 
   beforeEach(function (done) {
@@ -24,7 +32,7 @@ describe('Posts', function () {
     });
   });
 
-  describe('GET /', function () {
+  describe('GET /posts', function () {
     it('should success', function (done) {
       request(app)
         .get('/posts')
@@ -35,12 +43,65 @@ describe('Posts', function () {
     it('should return all posts', function (done) {
       models.Post.create({title: "foo", content: "bar"}, function (err, expected) {
         request(app)
-        .get('/posts')
+          .get('/posts')
+          .expect(200)
+          .expect(function (res) {
+            body = JSON.parse(res.text);
+            expect(body.length).to.eq(1);
+            var post = body[0];
+            expect(post.id).to.eq(expected._id.toString());
+            expect(post.title).to.eq(expected.title);
+            expect(post.content).to.eq(expected.content);
+          })
+          .end(done);
+      });
+    });
+
+    it('should filter with categoryId', function (done) {
+      models.Post.create([
+        {title: "post1", content: "post2", categoryId: categories[0]._id},
+        {title: "post1", content: "post2", categoryId: categories[1]._id}
+      ], function (err, expected) {
+        expect(err).to.be.null;
+        request(app)
+          .get('/posts?category_id=' + categories[0]._id.toString())
+          .expect(200)
+          .expect(function (res) {
+            body = JSON.parse(res.text);
+            expect(body.length).to.eq(1);
+            var post = body[0];
+            expect(post.id).to.eq(expected._id.toString());
+            expect(post.title).to.eq(expected.title);
+            expect(post.content).to.eq(expected.content);
+            expect(post.categoryId).to.eq(expected.categoryId.toString());
+          })
+          .end(done)
+      });
+    });
+  });
+
+  describe('GET /posts/:id', function () {
+    it('should fail with bad OID', function (done) {
+      request(app)
+        .get('/posts/foo')
+        .expect(500)
+        .end(done);
+    });
+
+    it('should return 404 with non existing OID', function (done) {
+      request(app)
+        .get('/posts/5441f8033e9b22331737fbd1')
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return existing post', function (done) {
+      models.Post.create({title: "foo", content: "bar"}, function (err, expected) {
+        request(app)
+        .get('/posts/' + expected.id.toString())
         .expect(200)
         .expect(function (res) {
-          body = JSON.parse(res.text);
-          expect(body.length).to.eq(1);
-          var post = body[0];
+          post = res.body;
           expect(post.id).to.eq(expected._id.toString());
           expect(post.title).to.eq(expected.title);
           expect(post.content).to.eq(expected.content);
@@ -48,5 +109,6 @@ describe('Posts', function () {
         .end(done);
       });
     });
+
   });
 });
